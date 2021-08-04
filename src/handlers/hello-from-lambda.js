e5ee54322a7e31bf6b5a2b2223ff28281029c743
Steve Smith <scsmith@users.noreply.github.com>
2021-08-04 16:18:02 +0100

    Respond to query string parameters

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -1,9 +1,13 @@
 exports.helloFromLambdaHandler = async (event, context) => {
   console.info(`${JSON.stringify(event)}`);
+  const name = event.queryStringParameters?.name || "world";
+  console.info(JSON.stringify(event.queryStringParameters))
+  console.info(`Name: ${name}`);
+
   return {
     "statusCode": 201,
     "body": JSON.stringify({
-      message: "Hello World"
+      message: `Hello ${name}`
     })
   };
 };
